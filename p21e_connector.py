49575e00eef308db4548d1988fede2907843d2dc
Stuart Malcolm <73795478+metadatadriven@users.noreply.github.com>
2023-09-11 17:36:50 +0100

    Create p21e_connector.py
    create file to demo P21E integration

---
+++ p21e_connector.py
@@ -0,0 +1,19 @@
+# P21E Launcher App
+ 
+import sys
+ 
+web_host  = sys.argv[1]
+data_pack = sys.argv[2]
+standard  = sys.argv[3]
+sdtm_ct   = sys.argv[4]
+adam_ct   = sys.argv[5]
+engine    = sys.argv[6]
+ 
+print("DOMIN-P21E ECLI PARAMETERS:")
+print (web_host)
+print (data_pack)
+print (standard)
+print (sdtm_ct)
+print (adam_ct)
+print (engine)
+print("---------------------------")
