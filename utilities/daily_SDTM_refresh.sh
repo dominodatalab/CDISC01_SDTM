c2d11cc65b23632c1b3bfbb38b7a3eedf005fbc1
Ross Sharp <ross.sharp@dominodatalab.com>
2025-07-03 09:38:46 -0700

    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/CDISC01_DataManagement/CDISC01_SDTM?executionId=6865932ab616683d10d955d0

--- utilities/daily_SDTM_refresh.sh
+++ utilities/daily_SDTM_refresh.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+# 1) Grab user JWT token 
+TOKEN=$(curl -s "$DOMINO_API_PROXY/access-token")
+
+# 2) Take snapshot of CDISC01_SDTMBLIND NetApp Volume
+curl -X POST "https://lsdemo.domino-eval.com/remotefs/v1/snapshots" \
+     -H "Content-Type: application/json" \
+     -H "Authorization: Bearer $TOKEN" \
+     -d '{
+       "volumeId": "7ad74260-7182-4c9e-9784-83e54eb44f94"
+     }'
+
+# 3) Take snapshot of CDISC01_UNSDTMBLIND NetApp Volume
+curl -X POST "https://lsdemo.domino-eval.com/remotefs/v1/snapshots" \
+     -H "Content-Type: application/json" \
+     -H "Authorization: Bearer $TOKEN" \
+     -d '{
+       "volumeId": "6423cdf9-3b6b-48d4-a948-cea816bdac40"
+     }'
